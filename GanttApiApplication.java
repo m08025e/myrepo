3dafe941491b6abbc07509860d2781658b7e5fb0
m08025e <129564853+m08025e@users.noreply.github.com>
2025-09-26 13:57:22 +0900

    Create GanttApiApplication.java

---
+++ GanttApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.ganttapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanttApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GanttApiApplication.class, args);
+    }
+}
